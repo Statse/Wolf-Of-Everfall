56e73a4e08189ec0361a63cbd160181bcc45d312
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-10-17 15:25:26 +0300

    posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Posty</title>
 </head>
-<body>
+    
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">Dash</a></li>
+            <li><a href="" class="p-3">Post</a></li>
+        </ul>
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Name</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+        </ul>
+    </nav>
     @yield("content")
 </body>
+
 </html>
