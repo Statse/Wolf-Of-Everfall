78880198c0ac9d2c2a61a9bfa4b919d914639f8b
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-10-18 21:13:35 +0300

    login erro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,21 +7,20 @@
                 <div class="mb-4">
                     <h1 class="w-full font-medium">Login</h1>
                 </div>
+
+                @if  (session('status'))
+                    <div class="text-red-500 mt-2 text-sm">{{session('status')}}</div>
+                @endif
+
                 <form action="{{route('login')}}") method="post">
                     @csrf
                     <div class="mb-4">
                         <label for="email" class="sr-only">Email</label>
                         <input type="text" id="email" name="email" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="{{old('email')}}" placeholder="Your email">
-                        @error('email')
-                            <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
-                        @enderror
                     </div>
                     <div class="mb-4">
                         <label for="password" class="sr-only">Password</label>
                         <input type="password" id="password" name="password" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="" placeholder="Your password">
-                        @error('password')
-                            <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
-                        @enderror
                     </div>
                     <div class="mb-4">
                         <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
