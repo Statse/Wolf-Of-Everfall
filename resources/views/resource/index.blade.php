343eaac27bc5e45e4171a0b0c759b06912b9ad7b
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-10-28 13:13:21 +0300

    dump slot item

--- resources/views/resource/index.blade.php
+++ resources/views/resource/index.blade.php
@@ -5,6 +5,11 @@
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
             <h1 class="text-xl">{{$resource->name}}</h1>
+            @if ($resource->slot_1)
+            <div>
+                {{var_dump($resource->slot_1)}}
+            </div>
+            @endif
         </div>
     </div>
 </section>
