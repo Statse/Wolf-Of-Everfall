5f6f3f7cf08f094a3307479201582008ca6a8a59
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-10-28 17:26:45 +0300

    table

--- resources/views/resource/index.blade.php
+++ resources/views/resource/index.blade.php
@@ -13,21 +13,33 @@
                     <p class="text-sm mt-auto">Category</p>
                 </div>
             </div>
-            <div class="p-6">
-                @if ($resource->slot_1)
-                    <div class="flex items-center">
-                        <div class="">{{ $resource->slot_1->amount }}</div>
-                        <select class="bg-gray-100 border-2 w-1/2 p-4 rounded-lg" name="ingredient" required id="ingredient">
-                            @foreach($resource->slot_1->ingredients as $ingredient)
-                                <option value="{{ $resource->id }}">{{ $ingredient}}</option>
-                            @endforeach
-                            @error('ingredient')
-                                <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
-                            @enderror
-                        </select>
-                    </div>
-                @endif
-            </div>
+            <table class="p-6 w-full table-auto">
+                <thead class="">
+                    <tr>
+                        <th>Ingredient</th>
+                        <th>Amount</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody class="">
+                    @if ($resource->slot_1)
+                        <tr class="flex items-center">
+                            <td>
+                                <select class="bg-gray-100 border-2 p-4 rounded-lg" name="ingredient" required id="ingredient">
+                                    @foreach($resource->slot_1->ingredients as $ingredient)
+                                        <option value="{{ $resource->id }}">{{ $ingredient}}</option>
+                                    @endforeach
+                                    @error('ingredient')
+                                        <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
+                                    @enderror
+                                </select>
+                            </td>
+                            <td class="">{{ $resource->slot_1->amount }}</td>
+                            <td>4.6</td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
         </div>
     </div>
 </section>
