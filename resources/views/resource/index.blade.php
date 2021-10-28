f46d9197e7ab48ad212731fd12e416ba2bc4176c
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-10-28 17:14:38 +0300

    styles

--- resources/views/resource/index.blade.php
+++ resources/views/resource/index.blade.php
@@ -8,13 +8,24 @@
                 @if ($resource->img_url)
                     <img class="bg-black border-white border-2" alt="resource icon" src="{{ $resource->img_url }}"/>
                 @endif
-                <h1 class="text-xl text-white ml-4">{{$resource->name}}</h1>
+                <div class="flex flex-col text-white ml-4">
+                    <h1 class="text-xl">{{$resource->name}}</h1>
+                    <p class="text-sm mt-auto">Category</p>
+                </div>
             </div>
             <div class="p-6">
                 @if ($resource->slot_1)
-                <div class="flex">
-                    data
-                </div>
+                    <div class="flex items-center">
+                        <div class="">{{ $resource->slot_1->amount }}</div>
+                        <select class="bg-gray-100 border-2 w-1/2 p-4 rounded-lg" name="ingredient" required id="ingredient">
+                            @foreach($resource->slot_1->ingredients as $ingredient)
+                                <option value="{{ $resource->id }}">{{ $ingredient}}</option>
+                            @endforeach
+                            @error('ingredient')
+                                <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
+                            @enderror
+                        </select>
+                    </div>
                 @endif
             </div>
         </div>
